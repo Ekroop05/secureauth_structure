977d2f0876dac25b4acc29dff0290ccecfc280c7
Ekroop05 <ekroopsingh2005@gmail.com>
2026-04-11 17:01:46 +0530

    fix correct backend url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://secureauth-structure.onrender.com/api",
+  baseURL: "https://secureauth-structure-1.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
